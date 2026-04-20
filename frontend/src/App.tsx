d57a465b361d274406ac5215c50d0127bc30e18f
swrig9 <swrig9@wgu.com>
2026-04-20 16:11:13 -0400

    basic nav bar added to top of every page along with basic css. Removed BaseURL from tsconfig as it is depreciating.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import PlayerPage from './pages/PlayerPage'
 import HomePage from './pages/HomePage'
 import HeroPage from './pages/HeroPage'
@@ -6,12 +7,15 @@ import HeroPage from './pages/HeroPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/player" element={<PlayerPage />} />
-      <Route path="/hero" element={<HeroPage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/player" element={<PlayerPage />} />
+        <Route path="/hero" element={<HeroPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   )
 }
 
